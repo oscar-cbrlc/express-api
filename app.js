4064d5e49ac93b33b08962d13fb7ab4ecde9f614
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-04-26 18:47:17 -0500

    Created an endpoint with object list

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
+app.get('/v1/explorers', (req, res) => {
+    console.log(`Api Explorers GET ALL requests ${new Date()}`)
+    const explorer1 = {id: 1, name: "mauri"}
+    const explorer2 = {id: 2, name: "omar"}
+    const explorer3 = {id: 3, name: "gaby"}
+    const explorer4 = {id: 4, name: "ivan"}
+    const explorers = [explorer1, explorer2, explorer3, explorer4]
+    res.status(200).json(explorers)
+})
+
 module.exports = app
