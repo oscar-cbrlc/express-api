a6b08ed321790208b5b393e75bcd3d0e8800938f
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-04-26 19:21:02 -0500

    Fixed info file path

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 const express = require('express')
-const explorers = require('info')
+const explorers = require('./info')
 const app = express()
 
 app.use(express.json())
 
 app.get('/v1/explorers', (req, res) => {
     console.log(`Api Explorers GET ALL requests ${new Date()}`)
-    const explorer1 = {id: 1, name: "mauri"}
-    const explorer2 = {id: 2, name: "omar"}
-    const explorer3 = {id: 3, name: "gaby"}
-    const explorer4 = {id: 4, name: "ivan"}
-    const explorers = [explorer1, explorer2, explorer3, explorer4]
     res.status(200).json(explorers)
 })
 
