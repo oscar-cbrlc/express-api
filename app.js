5ce10aa03b23752a5c3ccb36e879ada026651a60
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-04-26 19:19:30 -0500

    created info file

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const explorers = require('info')
 const app = express()
 
 app.use(express.json())
---
+++ info.js
@@ -0,0 +1,7 @@
+const explorer1 = {id: 1, name: "Carlo1"}
+const explorer2 = {id: 2, name: "Carlo2"}
+const explorer3 = {id: 3, name: "Carlo3"}
+const explorer4 = {id: 4, name: "Carlo4"}
+const explorers = [explorer1, explorer2, explorer3, explorer4]
+
+module.exports = explorers
